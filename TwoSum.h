b67aeff27f7e0d3d3d87fb352f984fa00dcfca7f
limgba <1805275257@qq.com>
2017-10-19 11:32:52 +0800

    Update TwoSum.h

--- TwoSum.h
+++ TwoSum.h
@@ -15,8 +15,8 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int i,j;
         vec.clear();
-        for (i = 0; i < nums.size(); i++) {
-            for (j = i + 1; j < nums.size(); j++) {
+        for (i = 0; i < nums.size(); ++i) {
+            for (j = i + 1; j < nums.size(); ++j) {
                 if (nums[i] + nums[j] == target) {
                     vec.push_back(i);
                     vec.push_back(j);
